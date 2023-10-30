2f3a333147151af4774650ffda19db9d76c834af
biadboze@gmail.com <biadboze@gmail.com>
2023-10-31 03:26:16 +0800

    add different endpoint

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const dns = require("dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post(
+  "api/shorturl",
+  (req, res, next) => {
+    next();
+  },
+  (req, res) => {}
+);
+
+app.get("api/shorturl/:short_url", (req, res) => {});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
